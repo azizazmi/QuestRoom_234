3658e05ae04a04a2054807ab50a7e07b1b3e3e78
Azizah A. Azmi <azizah.aurellia.ft23@mail.umy.ac.id>
2025-11-27 17:19:14 +0700

    feat: Add UI state and input validation to EntryViewModel

--- app/src/main/java/com/example/prak7_roomdatabase/viewmodel/EntryViewModel.kt
+++ app/src/main/java/com/example/prak7_roomdatabase/viewmodel/EntryViewModel.kt
@@ -1,8 +1,16 @@
 package com.example.prak7_roomdatabase.viewmodel
 
+import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import com.example.prak7_roomdatabase.repository.RepositoriSiswa
 
 class EntryViewModel (private val repositoriSiswa: RepositoriSiswa): ViewModel() {
+    var uiStateSiswa by mutableStateOf(UIStateSiswa())
+        private set
 
+    private fun validasiInput(uiState: DetailSiswa = uiStateSiswa.detailSiswa): Boolean {
+        return with(uiState) {
+            nama.isNotBlank() && alamat.isNotBlank() && telpon.isNotBlank()
+        }
+    }
 }
