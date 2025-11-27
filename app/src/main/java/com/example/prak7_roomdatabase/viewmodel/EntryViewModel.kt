669cfb7c21e08c2b04ea7436f7cb93664a9f6743
Azizah A. Azmi <azizah.aurellia.ft23@mail.umy.ac.id>
2025-11-27 17:16:22 +0700

    feat: Add initial `EntryViewModel` class

---
+++ app/src/main/java/com/example/prak7_roomdatabase/viewmodel/EntryViewModel.kt
@@ -0,0 +1,8 @@
+package com.example.prak7_roomdatabase.viewmodel
+
+import androidx.lifecycle.ViewModel
+import com.example.prak7_roomdatabase.repository.RepositoriSiswa
+
+class EntryViewModel (private val repositoriSiswa: RepositoriSiswa): ViewModel() {
+
+}
