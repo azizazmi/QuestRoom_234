33c91f24271721aa4d630cc20e9dcb9c50035c29
Azizah A. Azmi <azizah.aurellia.ft23@mail.umy.ac.id>
2025-12-04 13:27:59 +0700

    feat: Add `DestinasiEntry` object for navigation route and title resource.

---
+++ app/src/main/java/com/example/prak7_roomdatabase/view/route/DestinasiEntry.kt
@@ -0,0 +1,8 @@
+package com.example.prak7_roomdatabase.view.route
+
+import com.example.prak7_roomdatabase.R
+
+object DestinasiEntry {
+    override val route = "item_entry"
+    override val titleRes = R.string.entry_siswa
+}
